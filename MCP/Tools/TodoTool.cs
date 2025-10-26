bafeae824b61c07f2b7e61f728bdd7bfc0c38853
Torstein Egge <torstein.m.egge@twoday.com>
2025-10-26 09:52:16 +0100

    1.1 Add tool to list todos

--- MCP/Tools/TodoTool.cs
+++ MCP/Tools/TodoTool.cs
@@ -1,11 +1,25 @@
 ﻿using System.ComponentModel;
+using ModelContextProtocol;
 using ModelContextProtocol.Server;
+using TodoApi.Models;
+using TodoApi.Repositories;
 
 namespace TodoApi.MCP.Tools;
 
 [McpServerToolType]
-public class TodoTool()
+public class TodoTool(ITodoRepository todoRepo)
 {
     [McpServerTool, Description("Run this to test if the server is working")]
     public static string Echo(string message) => $"hello {message}";
+
+    [McpServerTool, Description("Tool to list all todos created by a specific user")]
+    public async Task<IEnumerable<Todo>> ListTodos(string creator)
+    {
+        if (string.IsNullOrWhiteSpace(creator))
+        {
+            throw new McpException("Missing creator parameter");
+        }
+        
+        return await todoRepo.GetByCreatorAsync(creator);
+    }
 }
